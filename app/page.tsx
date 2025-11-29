a4e5da57f2b3ebd628e3483f746e7b56cf51c7b9
Malinga Heshan <heshanjayalath66@gmail.com>
2025-11-29 17:51:59 +0530

    landing-page-complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,118 @@
+import { Button } from "@/components/ui/button";
+import {
+  BrainCogIcon,
+  EyeIcon,
+  GlobeIcon,
+  MonitorSmartphoneIcon,
+  ServerCogIcon,
+  ZapIcon,
+} from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+
+const features = [
+  {
+    name: "Store your PDF documents",
+    description:
+      "Keep all your important PDF documents in one secure place and easily access them whenever you need.",
+    icons: GlobeIcon,
+  },
+  {
+    name: "Blazing Fast Responses",
+    description:
+      "Experience lightning-fast responses to your queries, powered by advanced AI technology.",
+    icons: ZapIcon,
+  },
+  {
+    name: "Chat Memorisation",
+    description:
+      "Our AI remembers the context of your previous conversations, providing more accurate and relevant answers over time.",
+    icons: BrainCogIcon,
+  },
+  {
+    name: "Interactive PDF Viewer",
+    description:
+      "Easily view and interact with your PDF documents directly within the chat interface.",
+    icons: MonitorSmartphoneIcon,
+  },
+  {
+    name: "Clound Backup",
+    description:
+      "Your PDF documents are securely backed up in the cloud, ensuring they are always safe and accessible.",
+    icons: ServerCogIcon,
+  },
+  {
+    name: "Responsive across devices",
+    description:
+      "Access your PDF documents and chat with the AI from any device, whether it's a desktop, tablet, or smartphone.",
+    icons: EyeIcon,
+  },
+];
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="flex-1 overflow-scroll p-2 lg:p-5 bg-gradient-to-bl from-white to-orange-600">
+      <div className="bg-white py-24 sm:py-32 rounded-md drop-shadow-xl">
+        <div className="flex flex-col justify-center items-center mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-wxl sm:text-center">
+            <h2 className="text-base font-semibold lending-7 text-orange-600">
+              Your Interactive Document Companion
+            </h2>
+            <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Transform Your PDFs into interactive conversations
+            </p>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Introducing{" "}
+              <span className="font-bold text-orange-600">Chat with PDF.</span>
+              <br />
+              <br />
+              Upload your document, and our chatbot will answer questions,
+              summarize content, and answer all your Questions. Ideal for
+              everyone,
+              <span className="text-orange-600">Chat with PDF </span> turns
+              static documents into{" "}
+              <span className="font-bold">dynamic, conversations</span>enhancing
+              productivity 10x fold effortlessly.
+            </p>
+          </div>
+          <Button asChild className="mt-10 bg-gray-400">
+            <Link href="/dashboard">Get Started</Link>
+          </Button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+        <div className="relative overflow-hidden pt-14">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+              alt="App Screenshot"
+              src="https://www.appypie.com/blog/wp-content/uploads/2024/04/2-Set-Up-Your-Knowledge-Base.jpg"
+              width={2432}
+              height={1442}
+              className="mb-[-0%] rounded-xl shadow-2xl ring-1 ring-gray-900/10"
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            <div aria-hidden="true" className="relative">
+              <div className="absolute bottom-0 -inset-x-32 bg-gradient-to-t from-white/95 pt-[5%]">
+
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-    </div>
+        <div className="mx-auto mt-16 max-w-7xl px-6 sm:mt-20 md:mt-24 lg:px-8">
+          <dl className="mx-auto grid max-w-2xl grid-cols-1 gap-x-6
+          gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2
+          lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
+            {features.map((feature)=>(
+              <div className="relative pl-9">
+                <dt className="inline font-semibold text-gray-900">
+                  <feature.icons
+                    aria-hidden="true"
+                    className="absolute left-1 top-1 h-5 w-5 text-orange-600"
+                  />
+                </dt>
+                <dd>{feature.description}</dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </main>
   );
 }
