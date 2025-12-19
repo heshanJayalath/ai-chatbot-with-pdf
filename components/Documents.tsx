0f64eb05981bf00e5caaf79601f54ce9d864d7b4
Malinga Heshan <heshanjayalath66@gmail.com>
2025-12-19 23:01:08 +0530

    implemented upload ui feature

---
+++ components/Documents.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PlaceHolderDocument from "./PlaceHolderDocument";
+
+function Documents() {
+  return (
+    <div
+      className="flex flex-wrap p-5 bg-gray-100 justify-center
+    lg:justify-start rounded-sm gap-5 max-w-7xl mx-auto"
+    >
+      {/* Map through documents */}
+      <PlaceHolderDocument />
+    </div>
+  );
+}
+
+export default Documents;
